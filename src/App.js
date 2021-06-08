327738e61428514eb9a1472411b7a6ea9aa90133
claurennt <c.benedetti1989@gmail.com>
2021-06-08 19:45:06 +0200

    add/install Google Fonts, Bootstrap and Material UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Button from "@material-ui/core/Button";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+// import Typography from "@material-ui/core/Typography";
+
+import Header from "./Header";
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Raleway", "sans-serif"].join(","),
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
+      </div>
+    </ThemeProvider>
   );
 }
 
